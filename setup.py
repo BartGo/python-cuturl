e44d8153fb30cb1f84764680050ec3e6103a64bd
BartGo <BartGo@users.noreply.github.com>
2015-06-14 10:12:28 +0200

    fake-factory 0.5.0 -> 0.5.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open('README.rst') as readme:
             'bottle==0.12.8',
             'bottle-sqlalchemy==0.4.2',
             'click==4.0',
-            'fake-factory==0.5.1',
+            'fake-factory==0.5.2',
             'importlib==1.0.3', # needs to be added explicitely for OpenShift
             # 'Jinja2==2.7.3',
             # 'MarkupSafe==0.23',
