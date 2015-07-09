2090310e26ab160789c3cfa37147bfb5f43d00f1
BartGo <bartoszgo@gmail.com>
2015-07-09 22:25:50 +0200

    ...

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ virtualenv                      = os.path.join(virtenv, 'bin/activate_this.py')
 py_version                      = os.environ['OPENSHIFT_PYTHON_VERSION']
 py_cache                        = os.path.join(virtenv, 'lib', py_version, 'site-packages')
 os.environ['PYTHON_EGG_CACHE']  = os.path.join(py_cache)
-def show_var(name):
+def show_evar(name):
     try:
         print name + " = " + os.environ[""+name+""]
     except:
@@ -15,10 +15,10 @@ def show_var(name):
         pass
     return
 print "*** openshift environment variables:"
-show_var('OPENSHIFT_PYTHON_VERSION')
-show_var('PYTHON_EGG_CACHE')
-show_var('OPENSHIFT_PYTHON_DIR')
-show_var('OPENSHIFT_HOMEDIR')
+show_evar('OPENSHIFT_PYTHON_VERSION')
+show_evar('PYTHON_EGG_CACHE')
+show_evar('OPENSHIFT_PYTHON_DIR')
+show_evar('OPENSHIFT_HOMEDIR')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
     print "*** succeeded venv activation: " + virtualenv 
@@ -27,16 +27,24 @@ except IOError:
     pass
 sys.path.append("lib")
 from app import settings
-from app.routes import Routes as application
 print "*** app environment variables:"
-show_var('PROJECT_PATH')
-show_var('TEMPLATE_PATH') 
-show_var('STATIC_PATH')
+def show_pvar(name):
+    try:
+        print name + " = " + os.environ[""+name+""]
+    except:
+        print name + " : not found"
+        pass
+    return
+print "*** config variables:"
 os.environ['SQA_DBENGINE']=os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 'data', 'sqlite.db') 
-show_var('SQA_DBENGINE') # 'sqlite:///data//sqlite.db'
-show_var('SQA_ECHO') 
-show_var('SQA_KEYWORD') 
-show_var('SQA_CREATE') 
-show_var('SQA_COMMIT') 
-show_var('SQA_USE_KWARGS')
+show_pvar('PROJECT_PATH')
+show_pvar('TEMPLATE_PATH') 
+show_pvar('STATIC_PATH')
+show_pvar('SQA_DBENGINE') # 'sqlite:///data//sqlite.db'
+show_pvar('SQA_ECHO') 
+show_pvar('SQA_KEYWORD') 
+show_pvar('SQA_CREATE') 
+show_pvar('SQA_COMMIT') 
+show_pvar('SQA_USE_KWARGS')
+from app.routes import Routes as application
 print "*** wsgi.py finished"
