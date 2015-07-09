f0297f6121663733ae67a3a363a3c3276b587cf1
BartGo <bartoszgo@gmail.com>
2015-07-10 01:27:17 +0200

    ...

--- wsgi.py
+++ wsgi.py
@@ -55,5 +55,10 @@ print "*** starting the application"
 os.chdir(os.path.dirname(__file__))
 from app.routes import Routes as application
 
+from wsgiref.simple_server import make_server
+httpd = make_server('localhost', 8051, application)
+# Wait for a single request, serve it and quit.
+httpd.handle_request()
+
 print "*** wsgi.py finished"
 
