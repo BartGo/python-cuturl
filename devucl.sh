db7ae447d177c5b962f6e498e4f2a0d3fe067b22
BartGo <bartoszgo@gmail.com>
2015-07-11 01:18:11 +0200

    ...

--- devucl.sh
+++ devucl.sh
@@ -1,5 +1,7 @@
 # changelog from git log 
 # based on http://brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
+# consider also links to commit:
+#   git log --pretty=format:'<li> <a href="http://github.com/bartgo/cuturl/commit/%H">view commit &bull;</a> %s</li> ' -- reverse
 
 git log --date=iso --pretty=format:"* %cd - %s%n%b" \
 | grep -v ".* - \.\.\." | grep -v "^$" | grep -v ".* - Update .*" \
