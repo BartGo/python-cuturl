42d12c19c78aa639d04b835ff5a3fa9317715e13
BartGo <BartGo@users.noreply.github.com>
2015-12-20 01:28:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,16 +30,16 @@ with open('README.rst') as readme:
             'Beaker==1.7.0',
             #'bottle==0.12.8',
             'bottle-sqlalchemy==0.4.3',
-            'cherrypy==3.8.1',
+            'cherrypy==3.8.2',
             'click==6.2',
             "configparser>=3.3.0",
 	    "crashreporter==1.09",
             # 'importlib==1.0.3',  # may need to be added explicitely for OpenShift
             "logbook==0.12.3",
             "psycopg2", # if you want to use peewee with postgresql
-            'requests==2.8.1',
+            'requests==2.9.0',
             #'SQLAlchemy==1.0.8',
-            'alembic==0.8.3',
+            'alembic==0.8.4',
             'Mako==1.0.3',
             'python-slugify==1.1.4',
             'Unidecode==0.4.18'
