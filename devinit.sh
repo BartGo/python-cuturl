c05ccb9a8491c1bc8726786f9b9ca15700e96b71
BartGo <bartoszgo@gmail.com>
2016-05-10 14:47:59 +0200

    ...

--- devinit.sh
+++ devinit.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 IFS=$'\n\t'
 
 VENV_USED=1
-VENV_FIRST_INIT=1
+VENV_FIRST_INIT=0
 
 VENV_NAME=$( echo ${PWD##*/} | sed 's/[^a-z]*//g' ) # venv name is the current folder name (only lowercase characters)
 
 rm --recursive --force lib
 
-pip install --user --upgrade --requirement requirements-dev-base.txt
+pip install --user --upgrade --requirement requirements-dev-base.txt # TODO: sure for global?
 
 if [ $VENV_USED -eq 1 ]; then
   if [ $VENV_FIRST_INIT -eq 0 ]; then
@@ -20,7 +20,7 @@ if [ $VENV_USED -eq 1 ]; then
   pew in      $VENV_NAME pip install --upgrade --requirement requirements-dev.txt
   echo "pew in $VENV_NAME python -B manage.py runserver --debug True" > devrun.sh
   echo "pew in $VENV_NAME python -B manage.py alltests" > devtests.sh
-  echo "pew in $VENV_NAME pylint --output-format=parseable *.py app/ alembic/ features/ tests/" > devlint.sh
+  echo "pew in $VENV_NAME pylint --output-format=parseable app/ alembic/ features/ tests/ *.py" > devlint.sh
 else
   mkdir -p lib
   pip install --upgrade --requirement requirements-dev.txt --target lib
