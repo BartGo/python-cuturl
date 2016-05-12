8800e60d6589159d451605f1d3cfb03268e8c528
BartGo <BartGo@users.noreply.github.com>
2016-05-12 23:15:42 +0200

    Update home.py

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
+
 from bottle import Bottle, view, static_file
 
 from .. import settings
 
 home_app = Bottle()
-  
+
+# -- http://blog.bottlepy.org/2012/07/20/template-search-path-workaround.html
+import os
+import functools
+MY_TEMPLATE_PATH = [
+   os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.join('..','views'))),
+]
+tview = functools.partial(bottle.view, template_lookup=MY_TEMPLATE_PATH)
+# --
+
 @home_app.route('assets/<filepath:path>', name='assets')
 def static(filepath):
     return static_file(filepath, root=settings.STATIC_PATH)
     
-@home_app.route('/', apply=[view('index.html')])
+@home_app.route('/', apply=[tview('index.html')])
 def index():
   return {'get_url': home_app.get_url}
   
