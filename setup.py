0e185955d2544f0a60d2345052bd5835b4228467
BartGo <BartGo@users.noreply.github.com>
2016-10-12 18:25:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,21 +28,21 @@ with open('README.rst') as readme:
                               "assets/jquery/js/*.*"]},
         install_requires=[
             'Beaker==1.8.0',
-            'bottle==0.12.9',
+            'bottle==0.12.10',
             'bottle-sqlalchemy==0.4.3',
-            'cherrypy==8.1.0',
+            'cherrypy==8.1.2',
             'click==6.6',
             "configparser==3.5.0",
             #"crashreporter", # somehow does not like setuptools==21.0.0
-            "fake-factory==0.6.0",
+            "fake-factory==0.7.2",
             "funcsigs==1.0.2",
             # "importlib",  # may need to be added explicitely for OpenShift; is it OK for Python 3??
-            "ipaddress==1.0.16", # may need to be added explicitely for Drone.io, probably due to fake-factory
+            "ipaddress==1.0.17", # may need to be added explicitely for Drone.io, probably due to fake-factory
             "logbook==1.0.0",
             #"psycopg2", # if you want to use peewee with postgresql; note that it cannot be directly used with pypy
             'requests==2.11.1',
-            'SQLAlchemy==1.0.15',
-            'alembic==0.8.7',
+            'SQLAlchemy==1.1.1',
+            'alembic==0.8.8',
             'Mako==1.0.4',
             'python-slugify==1.2.1',
             'Unidecode==0.4.19'
