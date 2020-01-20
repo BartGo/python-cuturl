0b20a38a678912124a7f4bdf0b824fed96eb5ac8
BartGo <bartoszgo@gmail.com>
2020-01-21 00:14:21 +0100

    ...

--- devinit.sh
+++ devinit.sh
@@ -7,7 +7,6 @@ export DATABASE_URL="postgresql+psycopg2://cuturl:cuturl@localhost:5432/python-c
 
 python -m pip install --user --upgrade --requirement requirements-global.txt
 python -m virtualenv --clear --quiet ./env 
-python -m virtualenv --clear --quiet ./dnv 
 
 if [ "$OSTYPE" == "msys" ] ; then
   PYVE="Scripts"
@@ -15,18 +14,18 @@ else
   PYVE="bin"
 fi
 
-./dnv/$PYVE/pip install --upgrade --requirement requirements-dev.txt
+./env/$PYVE/pip install --upgrade --requirement requirements-dev.txt
 ./env/$PYVE/pip install --upgrade --requirement requirements.txt
 
 echo "export DATABASE_URL=$DATABASE_URL"          > devtests.sh
-echo "./env/$PYVE/python -B manage.py unittests" >> devtests.sh
+echo "./env/$PYVE/python -B manage.py tests"      >> devtests.sh
 echo "printf '\nRunning feature tests\n\n'"      >> devtests.sh
-echo "./dnv/$PYVE/behave"                        >> devtests.sh
+echo "./env/$PYVE/behave"                        >> devtests.sh
 
 echo "export DATABASE_URL=$DATABASE_URL"          > devrun.sh
 echo "./env/$PYVE/python -B manage.py runserver" >> devrun.sh
 
-echo "./dnv/$PYVE/pylint --output-format=parseable app/ alembic/ features/ tests/ *.py" > devlint.sh
+echo "./env/$PYVE/pylint --output-format=parseable app/ alembic/ features/ tests/ *.py" > devlint.sh
 
 chmod +x ./devrun.sh
 chmod +x ./devtests.sh
--- manage.py
+++ manage.py
@@ -10,6 +10,69 @@ from app.routes import app
 def cmds():
     pass
 
+
+def unittest_body():
+    # TODO: run tests on a perishable db
+    import unittest
+    loader = unittest.TestLoader()
+    tests = loader.discover('tests')
+    test_runner = unittest.runner.TextTestRunner()
+    test_runner.run(tests)
+
+
+"""
+def webtest_body():
+
+    click.echo("\nRunning web tests...")
+
+    import uuid
+    from webtest import TestApp
+    test_app  = TestApp(app)
+    all_tests = 1
+    nr_tests  = 0
+
+    # TODO: implement api, test json response
+    resp = test_app.get('/list/api')
+    assert resp.status == '200 OK'
+    assert resp.status_int == 200
+    assert resp.content_type == 'application/json'
+    nr_tests += 1
+
+    # save a link into db
+    resp = test_app.get('/list')
+    assert resp.status == '200 OK'
+    assert resp.status_int == 200
+    form = resp.forms[0]
+    fake = Factory.create()
+    ex_url = fake.url() # "http://" + uuid.uuid1().urn + ".ex"
+    ex_name = fake.name() # uuid.uuid1()
+    form["url-input"].value = ex_url
+    form["comment-input"].value = ex_name
+    resp = form.submit("save")
+    assert resp.status == '302 Found'
+    assert resp.status_int == 302
+    nr_tests += 1
+
+    summary = "\nRun " + str(nr_tests) + "/" + str(all_tests)
+    click.echo(summary)
+"""
+
+@cmds.command()
+def unittests():
+    unittest_body()
+
+
+#@cmds.command()
+#def webtests():
+#    webtest_body()
+
+
+@cmds.command()
+def tests():
+    #webtest_body()
+    unittest_body()
+
+
 @cmds.command()
 @click.option('--port', default=os.environ.get('PORT', 5000), type=int,
               help=u'Set application server port!')
