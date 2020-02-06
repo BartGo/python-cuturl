993f1b74ce55908b4c6174a6a607b95d7d2c5f96
BartGo <BartGo@users.noreply.github.com>
2020-02-06 23:35:31 +0100

    Update devinit.sh

--- devinit.sh
+++ devinit.sh
@@ -17,24 +17,27 @@ fi
 ./env/$PYVE/pip3 install --upgrade --requirement requirements.txt
 
 echo "export DATABASE_URL=$DATABASE_URL"           > devtests.sh
-echo "export LC_ALL=en_US.utf-8"		  >> devtests.sh
-echo "export LANG=en_US.utf-8"			  >> devtests.sh
+#echo "export LC_ALL=en_US.utf-8"		  >> devtests.sh
+#echo "export LANG=en_US.utf-8"			  >> devtests.sh
+echo "export PYTHONIOENCODING=UTF-8"  >> devtests.sh
+
 echo "./env/$PYVE/python3 -B manage.py tests"     >> devtests.sh
 echo "printf '\nRunning feature tests\n\n'"       >> devtests.sh
 echo "./env/$PYVE/behave"                         >> devtests.sh
 
 echo "export DATABASE_URL=$DATABASE_URL"           > devrun.sh
-echo "export LC_ALL=en_US.utf-8"		  >> devrun.sh
-echo "export LANG=en_US.utf-8"			  >> devrun.sh
+#echo "export LC_ALL=en_US.utf-8"		  >> devrun.sh
+#echo "export LANG=en_US.utf-8"			  >> devrun.sh
+echo "export PYTHONIOENCODING=UTF-8"  >> devrun.sh
 echo "./env/$PYVE/python3 -B manage.py runserver" >> devrun.sh
 
-echo "./env/$PYVE/pylint --output-format=parseable app/ alembic/ features/ tests/ *.py" > devlint.sh
+#echo "./env/$PYVE/pylint --output-format=parseable app/ alembic/ features/ tests/ *.py" > devlint.sh
 
 chmod +x ./devrun.sh
 chmod +x ./devtests.sh
-chmod +x ./devlint.sh
 chmod +x ./devucl.sh
 chmod +x ./manage.py
+#chmod +x ./devlint.sh
 
 ./devtests.sh
 
